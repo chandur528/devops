7a17f2dc2ea8896c494e3b40c0bc6107ccf7e7e4
chandur528 <chandursupriya123@gmail.com>
2025-08-20 15:13:11 +0530

    Add files via upload

---
+++ HollowSquarePattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HollowSquarePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the square: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                // Print '*' at borders, space elsewhere
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
